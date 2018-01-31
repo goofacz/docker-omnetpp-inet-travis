3f54048f82358d0c8e8dea9cce02c67d23a636fe
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-01-31 19:22:33 +0100

    Run OMNET++ tests in travis.sh

--- travis.sh
+++ travis.sh
@@ -28,13 +28,32 @@ function build_stage {
 } # function build_stage
 
 
+function run_omnetpp_tests_stage {
+    if [ ! -d "/root/smile/tests" ]
+    then
+        echo "*** Skipping OMNET++ tests, directory \"/root/smile/tests\" was not found"
+        return
+    fi
+
+    echo "** Running OMNET++ tests"
+
+    cd /root/smile/tests
+    ./runtest
+    FAILURES_FOUND=`opp_test check -p smile * | grep "FAIL: 0" | wc -l`
+    if [ "$FAILURES_FOUND" -ne "0" ]
+    then
+        exit 1
+    fi
+} # run_omnetpp_tests_stage
+
 if [ -z "$SMILE_FRAMEWORK" ]
 then
-    echo "Building & testing SMILe framework"
+    echo "*** Building & testing component based on SMILe framework"
 else
-    echo "Building & testing component based on SMILe framework"
+    echo "*** Building & testing SMILe framework"
 fi
 
 build_stage
+run_omnetpp_tests_stage
 
 exit 0
